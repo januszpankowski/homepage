bcaaf4e29ddd8d69025b05057fe6093dca0819da
Janusz Pankowski <janusz.pankowski@onet.eu>
2019-06-11 16:23:10 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('siema');
+const name = 'Janusz';
+const age = 31;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('witaj przybyszu');
